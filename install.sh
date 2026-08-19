f62834176adcbb19f50d81bf7abea5320581feb0
JoshxB-T <joshtorrez320@gmail.com>
2026-08-18 22:37:03 -0500

    feat: added colored output and improved logic for installing packages

--- install.sh
+++ install.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-dependencies=("neovim" "hypr" "kitty" "neovim" "tmux" "waybar" "wofi" "zsh")
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[31m'
+NC='\033[0m'
 
-echo "Installing dependencies..."
-for dependency in "${dependencies[@]}"; do
-    echo "pacman -S $dependency"
+PACKAGES=(
+    "neovim"
+    "hyprland"
+    "kitty"
+    "tmux"
+    "waybar"
+    "wofi"
+    "zsh"
+)
+
+echo -e "${YELLOW}Installing packages...${NC}"
+for PACKAGE in "${PACKAGES[@]}"; do
+    if pacman -Qi ${PACKAGE} &> /dev/null ; then
+        echo -e "${GREEN}[ALREADY INSTALLED]${NC} $PACKAGE is already present on the system."
+    else
+        echo "pacman -S -y $package"
+
+        if [ $? -eq 1 ] ; then
+            echo -e "${GREEN}[SUCCESS]${NC} $PACKAGE installed successfully."
+        else
+            echo -e "${RED}[ERROR]${NC} Failed to install $PACKAGE."
+        fi
+    fi
 done
-echo "Completed installing dependencies."
-
-echo "Moving files..."
+echo -e "${GREEN}Completed installing packages.${NC}\n"
+echo -e "${YELLOW}Moving files...${NC}"
 echo "mv fastfetch $HOME/.config/fastfetch/"
 echo "mv neovim $HOME/.config/nvim/"
 echo "mv kitty $HOME/.config/kitty"
@@ -17,4 +39,4 @@ echo "mv waybar $HOME/.config/waybar"
 echo "mv wofi $HOME/.config/wofi"
 echo "mv zsh/.zshrc $HOME/.zshrc"
 echo "mv tmux/.tmux.conf $HOME/.tmux.conf"
-echo "Completed moving files."
+echo -e "${GREEN}Completed moving files.${NC}"
